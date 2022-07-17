51bf1a9420aef0178efb54f8ca41de37e35fba68
dillaheniputri <dillahenipertiwi@gmail.com>
2022-07-18 01:09:21 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+include 'functions.php';
+// Your PHP code here.
+
+// Home Page template below.
+?>
+
+<?=template_header('Home')?>
+
+<div class="content">
+	<h2>Holaaa</h2>
+	<p>Welcome temen-temen mahasiswa/i BTQ. selamat datang di web ku (dillaheni)</p>
+</div>
+
+<?=template_footer()?>
